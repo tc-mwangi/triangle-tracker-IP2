729735ff918c96744220266f4eb0af3a881f73dc
Loise Mwangi <wanjirumwangi.learn@gmail.com>
2018-11-30 08:32:41 +0300

    bdd breakdown in scripts.jss

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,16 @@
   //.Not a triangle
 
 //Behavior Driven Development BDD
+//===================================
+//Front-end/User Interface logic
+//1. Give brief a description and illustrations of types of triangle.
+//2. get user input (length a, length b, length c - pre-defined).
+//3.
+//4. display answer(with an image and name of the triangle.)
+
+//Back-end/Business logic
+//1. create/ make enter button active.
+//2. get input from user (length a, length b, length c - pre-defined).
+//2. evaluate properties of the 4 scenarios
+//3. create functions and arrays
+//4. alert user (answer)
