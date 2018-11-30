7aa4f339ba001260c9054637d3d3ec17e4734db7
Loise Mwangi <wanjirumwangi.learn@gmail.com>
2018-11-30 15:20:51 +0300

    add predictive text in input placeholder

---
+++ README.md
@@ -0,0 +1,9 @@
+#Triangle PI
+#Project Description
+#Author information
+#Setup Instructions
+#Behaviour Driven Development(BDD)
+#Technologies used
+#Contact information
+#License and copyright information
+#
--- js/scripts.js
+++ js/scripts.js
@@ -13,8 +13,22 @@
 //4. display answer(with an image and name of the triangle.)
 
 //Back-end/Business logic
-//1. create/ make enter button active.
-//2. get input from user (length a, length b, length c - pre-defined).
+//1. get input from user (length a, length b, length c - pre-defined).
+//2. create/ make enter button active.
 //2. evaluate properties of the 4 scenarios
 //3. create functions and arrays
 //4. alert user (answer)
+
+//1. get input from user (length a, length b, length c - pre-defined).
+
+//var button = document.getElementById("btn");
+//var form = document.getElementByClass("form");
+
+//button.onclick = function(){
+    //var body = document.getElementsByTagName("body")[0];
+    //body.style.backgroundColor = "blue";
+//}
+var button = document.getElementById("btn");
+button.addEventListener("click", function() {
+  console.log("Here is the answer");
+};
